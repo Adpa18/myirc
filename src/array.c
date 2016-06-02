639e2b658ff907f994b7140cf17bfabeab5bb625
Adrien WERY <adrien.wery@epitech.eu>
2016-06-02 09:21:30 +0200

    fix client

--- src/array.c
+++ src/array.c
@@ -86,7 +86,7 @@ inline char     *merge(const char **array, const char *delim)
     for (int i = 0; i < array_len(array); ++i)
     {
         tmp = dest;
-        dest = concat(3, dest, (dest) ? delim : NULL, array[i]);
+        dest = concat(3, dest, (dest) ? delim : "", array[i]);
         if (tmp)
             free(tmp);
     }
